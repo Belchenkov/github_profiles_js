d98280dcbd00b7d84afe1476bc719f59918b46d8
Aleksey Belchenkov <u608110@gmail.com>
2021-01-12 14:44:51 +0300

    Fetch Profile Data from Github

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
+const API_URL = 'https://api.github.com/users/';
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);
+
+        search.value = '';
+    }
+});
+
+async function getUser(username) {
+    try {
+        const { data } = await axios.get(API_URL + username);
+        console.log(data)
+    } catch (err) {
+        console.error(err);
+    }
+}
